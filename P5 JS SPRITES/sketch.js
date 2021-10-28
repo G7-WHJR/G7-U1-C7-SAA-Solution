7ac5d6ce89bc2a1fb8c4ee4e4aa24bd609bc3eaf
unknown <raveen.anand@whitehatjr.com>
2021-10-28 17:10:07 +0530

    v2

--- P5 JS SPRITES/sketch.js
+++ P5 JS SPRITES/sketch.js
@@ -1,10 +1,19 @@
 var box;
 function setup() {
   createCanvas(400, 400);
-  box = createSprite(200, 200, 40, 40);
+
+  //for SAA2:
+  box = createSprite(40, 40, 40, 40);
   box.shapeColor = "white";
   box.velocityX=0.5;
   box.velocityY=0.5;
+
+  //For SAA1:
+  /*
+  box = createSprite(300, 40, 40, 40);
+  box.shapeColor = "white";
+  box.velocityX=-0.5;
+  */
 }
 function draw() {
   background("orange");
